8747df971f75334354d0485f903d4ce901b97685
Alon Hoffman <a.hoffman.lon@gmail.com>
2022-11-09 12:42:46 +0200

    Header css

--- cmps/app-header.cmp.js
+++ cmps/app-header.cmp.js
@@ -1,11 +1,25 @@
 export default {
-	template: `
-        <header class="app-header">
-            <h1>AppSus</h1>
-            <nav>
-                <router-link to="/">Home</router-link> | 
-                <router-link to="/about">About</router-link>
-            </nav>
-        </header>
+    template: `
+       
+
+       <header>
+      <div class="main-header main-layout flex justify-between align-center">
+        <h2 class="logo">
+          AppSus
+        </h2>
+        <ul class="nav-bar flex clean-list">
+          <li><router-link :to="'/notes'" class="flex justify-center align-center">
+          Notes
+        </a></li>
+          <li><router-link :to="'/mail'" class="flex justify-center align-center">
+          Mail
+        </a></li>
+          <li>
+            <a href="#" class="flex justify-center align-center">About</a>
+          </li>
+        </ul>
     `,
+    created() {
+        console.log("app-heder")
+    }
 }
