11955155322bb9287894cb3e95fcf30b6a0bb5d5
Nir Roiter <aniroiter@gmail.com>
2022-11-10 18:16:25 +0900

    setting grid

--- apps/keep/services/note.service.js
+++ apps/keep/services/note.service.js
@@ -44,7 +44,7 @@ function _createNotes() {
         id: 'n101',
         type: 'note-txt',
         isPinned: true,
-        info: { txt: 'Fullstack Me Baby!' },
+        info: { txt: 'What is Lorem IpsumLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.' },
       },
       {
         id: 'n102',
