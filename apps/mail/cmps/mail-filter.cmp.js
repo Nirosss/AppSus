9f019e7d03ba8102b4b77331c6663cd5d21b4210
Alon Hoffman <a.hoffman.lon@gmail.com>
2022-11-12 17:23:48 +0200

    wip

--- apps/mail/cmps/mail-filter.cmp.js
+++ apps/mail/cmps/mail-filter.cmp.js
@@ -6,21 +6,35 @@ export default {
                 v-model="filterBy.name" 
                 type="text" 
                 placeholder="Search" />
-                <div>Draft Folder
-                <input type="checkbox" @input="filter" v-model="filterBy.draft"
-                         /></div>
+
+                <button @click="setFolder('draft')">Draft Folder</button>
+                <button @click="setFolder('sent')">Sent Folder</button>
+                <button @click="setFolder('starred')">Starred Folder</button>
+                <button @click="setFolder('inbox')">Draft inbox</button>
+
+                <div>Draft Folder<input type="checkbox" @input="filter" v-model="filterBy.draft" /></div>
+                <div>Sent Folder<input type="checkbox" @input="filter" v-model="filterBy.sent" /></div>
+                <div>Starred Folder<input type="checkbox" @input="filter" v-model="filterBy.Starred" /></div>
+                <div>inbox<input type="checkbox" @input="filter" v-model="filterBy.inbox" /></div>
+                
+                        
         </section>
     `,
     data() {
         return {
             filterBy: {
                 name: '',
-                draft: false
+                folder: null
             }
         }
     },
     methods: {
+        setFolder(searchFolder) {
+            this.filterBy.folder = searchFolder
+            this.filter()
+        },
         filter() {
+            console.log(this.filterBy)
             this.$emit('filter', this.filterBy)
         }
     },
