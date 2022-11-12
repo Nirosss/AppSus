0777cc127d74c765334156df0ff258b37bb0071c
Nir Roiter <aniroiter@gmail.com>
2022-11-13 00:52:21 +0900

    closing tag removal

--- views/app-home.cmp.js
+++ views/app-home.cmp.js
@@ -1,7 +1,7 @@
 export default {
   template: `
         <section class="home-page flex">
-            <img class="home-img" src="/assets/img/pngwing.com (3).png" alt=""></img>
+            <img class="home-img" src="/assets/img/pngwing.com (3).png" alt=""/>
             <span class="home-content"> Your Favorite Apps Got W(horse)</span>
 
         </section>
