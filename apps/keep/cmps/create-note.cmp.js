be93c7ab3d2ffc73add2fd5483c43aef73902d6d
Nir Roiter <aniroiter@gmail.com>
2022-11-13 00:29:54 +0900

    changed buttons

--- apps/keep/cmps/create-note.cmp.js
+++ apps/keep/cmps/create-note.cmp.js
@@ -10,10 +10,10 @@ export default {
         <div class="new-note-buttons flex">
         <button class="save-new-note" v-if="typedTxt" @click="createNote">Save</button>
           <div v-if="!typedTxt" class="new-note-btn-container flex">
+            <button @click="setType('txt')" style="background-image: url('../../../../assets/img/buttons/text16x16.png')"  title="Create Text Note"></button>
            <button @click="setType('img')" style="background-image: url('../../../../assets/img/buttons/uploadimg16x16.png')" title="Create Image note"></button>
             <button @click="setType('video')" style="background-image: url('../../../../assets/img/buttons/video16x16.png')" title="Create video note"></button>
-            <button @click="setType('txt')" style="background-image: url('../../../../assets/img/buttons/text16x16.png')"  title="Create Text Note"></button>
-            <button @click="setType('list')" style="background-image: url('../../../../assets/img/buttons/todo20x20.png')" title="Create list note"></button>
+            <button @click="setType('list')" style="background-image: url('../../../../assets/img/buttons/todo16x16.png')" title="Create list note"></button>
          </div>
         </div>
     </div>
